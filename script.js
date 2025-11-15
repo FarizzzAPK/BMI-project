d4d480ee552b92e0b79c493480b2bc9919b00e6b
Farizzz <113644816+FarizzzAPK@users.noreply.github.com>
2025-11-15 20:09:25 +0200

    update the status

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function calculateBMI() {
     document.getElementById("bmiValue").textContent = bmi;
 
     let status = "";
-    if (bmi < 18.5) status = "Underweight";
-    else if (bmi < 25) status = "Normal Weight";
-    else if (bmi < 30) status = "Overweight";
-    else status = "Obese";
+    if (bmi < 18.5) status = "Underweight, انت كدا هتطير في الهواء يسطا";
+    else if (bmi < 25) status = "Normal Weight, انت اصلي وتمام التمام";
+    else if (bmi < 30) status = "Overweight, بص الجيم مش للمفركشين بس";
+    else status = "Obese,يا دبابة انت محتاج حزام امان";
 
     document.getElementById("bmiStatus").textContent = status;
 }
